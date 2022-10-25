914ca6f8ec48a8247a01da3f0246a713b7eb7641
Ad4m1 <49112781+2374579255@users.noreply.github.com>
2022-10-25 11:26:13 +0800

    Add files via upload

--- pc/acg.php
+++ pc/acg.php
@@ -1,1 +1,49 @@
+<?php
+//读取文本
+$str = explode("\n", file_get_contents('img.txt'));
+$k = rand(0,count($str));
+$sina_img = str_re($str[$k]);
 
+$size_arr = array('kf', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
+$size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
+if(!in_array($size, $size_arr)){
+	$size = 'large';
+}
+$url = 'https://tva1.sinaimg.cn/'.$size.'/'.$sina_img.'.jpg';
+//解析结果
+$result=array("code"=>"200","acgurl"=>"$url");
+//Type Choose参数代码
+$type=$_GET['return'];
+switch ($type)
+{   
+   
+//格式解析                             
+case 'json':
+$path = "$url";
+$pathinfo = pathinfo($path);
+$imageInfo = getimagesize($url);  
+$result['width']="$imageInfo[0]";  
+$result['height']="$imageInfo[1]";
+$result['size']="$pathinfo[extension]";    
+header('Content-type:text/json');
+echo json_encode($result);
+break;
+//格式解析                             
+case 'img':
+$img = file_get_contents($url,true);
+header("Content-Type: image/jpeg;");
+echo $img;
+break;
+//IMG
+default:
+header("Location:".$result['acgurl']);
+break;
+}
+function str_re($str){
+  $str = str_replace(' ', "", $str);
+  $str = str_replace("\n", "", $str);
+  $str = str_replace("\t", "", $str);
+  $str = str_replace("\r", "", $str);
+  return $str;
+}
+?>
